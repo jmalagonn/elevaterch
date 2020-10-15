5da99443d3d460c8646f5258c697d3d0917b9330
jmalagonn <nicolas0915@hotmail.com>
2020-10-15 11:55:38 -0500

    Add mail info

--- wp-content/themes/elevaterch/footer.php
+++ wp-content/themes/elevaterch/footer.php
@@ -7,6 +7,7 @@
                 <div class="col-md-6 text-center">
                     <h6>CONTÁCTENOS</h6>
                     <p class="m-0">
+                        <a class="footer-link" href="mailto:info@elevaterchingenieria.com">Email: info@elevaterchingenieria.com</a><br>
                         <a class="footer-link" href="tel:+5717359423">Teléfono: (57) 1 735 94 23</a><br>
                         <a class="footer-link" href="tel:+573112890608">Celular: 311 289 06 08</a><br>
                         Dirección: Calle 42 sur # 8 A 89 e<br>
